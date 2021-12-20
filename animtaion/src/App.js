c9b99f87d05efecb5f08fe382bcb637a43cad138
mingg123 <spgb123spgb@gmail.com>
2021-12-20 11:57:17 +0900

    [BugFix] 구름하나가 움직이지 않던 문제 해결, star 정상 동작 하도록 수정

--- animtaion/src/App.js
+++ animtaion/src/App.js
@@ -3,19 +3,24 @@ import forestImg from "./image/forest.png";
 import cloud1Img from "./image/cloud1.png";
 import cloud2Img from "./image/cloud2.png";
 import cloud3Img from "./image/cloud3.png";
+import { useEffect } from "react";
 function App() {
+  useEffect(() => {
+    {
+      star();
+    }
+  }, []);
   return (
     <div className="App">
       <div className="scene">
-        <div class="moon"></div>
-        <img src={forestImg} class="forest"></img>
-        <img src={cloud1Img} class="cloud1"></img>
-        <img src={cloud2Img} class="cloud2"></img>
-        <img src={cloud3Img} class="cloud3"></img>
-        <img src={cloud1Img} class="cloud4"></img>
-        <img src={cloud2Img} class="cloud5"></img>
-        <img src={cloud3Img} class="cloud6"></img>
-        {star()}
+        <div className="moon"></div>
+        <img src={forestImg} className="forest"></img>
+        <img src={cloud1Img} className="cloud1"></img>
+        <img src={cloud2Img} className="cloud2"></img>
+        <img src={cloud3Img} className="cloud3"></img>
+        <img src={cloud1Img} className="cloud4"></img>
+        <img src={cloud2Img} className="cloud5"></img>
+        <img src={cloud3Img} className="cloud6"></img>
       </div>
     </div>
   );
@@ -35,7 +40,6 @@ function star() {
     star.style.top = y + "px";
     star.style.width = 1 + size + "px";
     star.style.height = 1 + size + "px";
-
     star.style.animationDuration = 5 + duration + "s";
     star.style.animationDelay = +duration + "s";
     scene.appendChild(star);
