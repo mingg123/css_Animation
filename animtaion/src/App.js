2e9a1f87a2e61e2d8920a986bf935e140adbe329
mingg123 <spgb123spgb@gmail.com>
2021-12-20 13:34:50 +0900

    refactor : 코드 컴포넌트화 및  router 버전 수정, 기타 수정 1. 구름 속도 빠르게

--- animtaion/src/App.js
+++ animtaion/src/App.js
@@ -4,47 +4,35 @@ import cloud1Img from "./image/cloud1.png";
 import cloud2Img from "./image/cloud2.png";
 import cloud3Img from "./image/cloud3.png";
 import { useEffect } from "react";
+import ForestComponent from "./components/ForestComponent";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function App() {
-  useEffect(() => {
-    {
-      star();
-    }
-  }, []);
   return (
-    <div className="App">
-      <div className="scene">
-        <div className="moon"></div>
-        <img src={forestImg} className="forest"></img>
-        <img src={cloud1Img} className="cloud1"></img>
-        <img src={cloud2Img} className="cloud2"></img>
-        <img src={cloud3Img} className="cloud3"></img>
-        <img src={cloud1Img} className="cloud4"></img>
-        <img src={cloud2Img} className="cloud5"></img>
-        <img src={cloud3Img} className="cloud6"></img>
+    <BrowserRouter>
+      <div className="App">
+        Example
+        <li className="list">
+          <Link to="/">Home </Link>
+        </li>
+        <li className="list">
+          <Link to="/forest">Forest </Link>
+        </li>
+        <Switch>
+          <Route path="/" component={ForestComponent} />
+          <Route path="/forest" component={ForestComponent} />
+          <Redirect to={ForestComponent} />
+        </Switch>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
-function star() {
-  let count = 500;
-  let scene = document.querySelector(".scene");
-  let i = 0;
-  while (i < count) {
-    var star = document.createElement("i");
-    let x = Math.floor(Math.random() * window.innerWidth);
-    let y = Math.floor(Math.random() * window.innerHeight);
-    let duration = Math.random() * 10;
-    let size = Math.random() * 2;
-    star.style.left = x + "px";
-    star.style.top = y + "px";
-    star.style.width = 1 + size + "px";
-    star.style.height = 1 + size + "px";
-    star.style.animationDuration = 5 + duration + "s";
-    star.style.animationDelay = +duration + "s";
-    scene.appendChild(star);
-    i++;
-  }
-}
 export default App;
